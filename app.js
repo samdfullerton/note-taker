d78a221a53907f149ca24f534ad3927c426a201e
Sam Fullerton <Samdfullerton@gmail.com>
2020-10-25 11:59:32 -0500

    initial file setup

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var htmlroutes = require("./routes/html-routes")
+var app = express()
+var PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+// app.use("/api", apiroutes);
+app.use("/", htmlroutes);
+app.listen(PORT, function() {
+    console.log(`App listening on PORT: 
+    ${PORT}`);
+}) 
+

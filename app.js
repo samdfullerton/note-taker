0718811194a0fe21e972f81a20b9107e5bef3aee
Sam Fullerton <Samdfullerton@gmail.com>
2020-10-27 21:19:29 -0500

    updated api-routes

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 var express = require("express");
-var htmlroutes = require("./routes/html-routes")
+var htmlroutes = require("./routes/html-routes");
+var apiroutes = require("./routes/api-routes");
 var app = express()
 var PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
-// app.use("/api", apiroutes);
+app.use("/", apiroutes);
 app.use("/", htmlroutes);
 app.listen(PORT, function() {
     console.log(`App listening on PORT: 
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,3 +1,20 @@
-// var path = require("path");
-// var router = require ("express").Router();
+var path = require("path");
+var router = require ("express").Router();
+var fs =require("fs");
+const { patch } = require("./html-routes");
+    
+router.get("/api/notes",(req, res) => {
+    fs.readFile(path.join(__dirname, "../db/db.json"), "utf8", (err, data) => {
+        if (err) throw err;
+        console.log(data);
+        res.status(200).json(data)
+    })
+})
+router.post("/api/notes", (req, res) => {
+  const newNote = req.body //this will be an object that comes from front end requeat
+    fs.write("filename", newNote, (err, data) => {
 
+
+    })
+}) 
+module.exports = router;
